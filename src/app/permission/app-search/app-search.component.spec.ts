a21cf40721580db5d73352aea57bf2e096df2229
Christian Badura <christian.badura@capgemini.com>
2024-05-15 10:58:26 +0200

    fix: improve tests for app search

--- src/app/permission/app-search/app-search.component.spec.ts
+++ src/app/permission/app-search/app-search.component.spec.ts
@@ -1,6 +1,8 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { Router } from '@angular/router'
+import { ActivatedRoute } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
 import { TranslateTestingModule } from 'ngx-translate-testing'
 import { DataViewModule } from 'primeng/dataview'
@@ -12,7 +14,7 @@ import {
   WorkspaceAbstract,
   WorkspacePageResult
 } from 'src/app/shared/generated'
-import { AppSearchComponent } from './app-search.component'
+import { App, AppSearchComponent } from './app-search.component'
 import { HttpResponse } from '@angular/common/http'
 
 const wsAbstract: WorkspaceAbstract = {
@@ -29,6 +31,8 @@ const wsPageRes: WorkspacePageResult = {
 describe('AppSearchComponent', () => {
   let component: AppSearchComponent
   let fixture: ComponentFixture<AppSearchComponent>
+  let mockActivatedRoute: ActivatedRoute
+  let mockRouter = { navigate: jasmine.createSpy('navigate') }
 
   const appApiSpy = jasmine.createSpyObj<ApplicationAPIService>('ApplicationAPIService', ['searchApplications'])
   const wsApiSpy = jasmine.createSpyObj<WorkspaceAPIService>('WorkspaceAPIService', ['searchWorkspaces'])
@@ -47,7 +51,9 @@ describe('AppSearchComponent', () => {
       ],
       providers: [
         { provide: ApplicationAPIService, useValue: appApiSpy },
-        { provide: WorkspaceAPIService, useValue: wsApiSpy }
+        { provide: WorkspaceAPIService, useValue: wsApiSpy },
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents()
@@ -63,5 +69,101 @@ describe('AppSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  /**
+   * UI Events
+   */
+  it('should navigate to detail page when a tile is clicked', () => {
+    const app: App = {
+      appId: 'appId',
+      isApp: true,
+      appType: 'APP'
+    }
+    const product: App = {
+      appId: 'appId',
+      isApp: true,
+      appType: 'PRODUCT'
+    }
+
+    component.onAppClick(app)
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['./', 'app', app.appId], { relativeTo: undefined })
+
+    component.onAppClick(product)
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['./', 'app', app.appId], { relativeTo: undefined })
+  })
+
+  it('should update filterBy and filterValue onQuickFilterChange: ALL', () => {
+    component.onQuickFilterChange({ value: 'ALL' })
+
+    expect(component.filterBy).toBe(component.filterValueDefault)
+    expect(component.filterValue).toBe('')
+  })
+
+  it('should update filterBy and filterValue onQuickFilterChange: other', () => {
+    component.onQuickFilterChange({ value: 'other' })
+
+    expect(component.filterValue).toBe('other')
+    expect(component.filterBy).toBe('appType')
+  })
+
+  it('should disable name input field is app type on search is ALL', () => {
+    component.onAppTypeFilterChange({ value: 'ALL' })
+    expect(component.appSearchCriteriaGroup.controls['name'].disabled).toBeTrue()
+
+    component.onAppTypeFilterChange({ value: 'Apps' })
+    expect(component.appSearchCriteriaGroup.controls['name'].enabled).toBeTrue()
+  })
+
+  it('should call filter table onFilterChange', () => {
+    component.dv = jasmine.createSpyObj('test', ['filter'])
+
+    component.onFilterChange('test')
+
+    expect(component.filter).toBe('test')
+    expect(component.dv?.filter).toHaveBeenCalledWith('test', 'contains')
+  })
+
+  it('should set correct values onSortChange', () => {
+    component.onSortChange('field')
+
+    expect(component.sortField).toEqual('field')
+  })
+
+  it('should set correct values onSortDirChange', () => {
+    component.onSortDirChange(true)
+
+    expect(component.sortOrder).toEqual(-1)
+  })
+
+  it('should set correct values onSortDirChange', () => {
+    component.onSortDirChange(false)
+
+    expect(component.sortOrder).toEqual(1)
+  })
+
+  it('should searchApps when search button is clicked', () => {
+    spyOn(component, 'searchApps')
+
+    component.onSearch()
+
+    expect(component.searchApps).toHaveBeenCalled()
+  })
+
+  it('should reset search criteria group and assign empty array to apps observable', (done) => {
+    spyOn(component.appSearchCriteriaGroup, 'reset')
+
+    component.onSearchReset()
+
+    expect(component.appSearchCriteriaGroup.reset).toHaveBeenCalledOnceWith({ appType: 'ALL' })
+    component.apps$.subscribe({
+      next: (res) => {
+        if (res) {
+          expect(res).toEqual([] as App[])
+        }
+        done()
+      },
+      error: done.fail
+    })
   })
 })
