4538fb7d92ad1f7c3b596c664a68ce00d6452872
Christian Badura <christian.badura@capgemini.com>
2024-08-07 12:17:34 +0200

    fix: user roles perms test setup

--- src/app/remotes/user-roles-permissions/user-roles-permissions.component.spec.ts
+++ src/app/remotes/user-roles-permissions/user-roles-permissions.component.spec.ts
@@ -1,271 +1,133 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { TableModule } from 'primeng/table'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import { Router } from '@angular/router'
+import { of, ReplaySubject } from 'rxjs'
 
-// import { RolesPermissionsComponent } from './roles-permissions.component'
-// import { HttpClientTestingModule } from '@angular/common/http/testing'
-// import { HttpClient } from '@angular/common/http'
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
-// import { HttpLoaderFactory } from '../../shared/shared.module'
+import { AppConfigService } from '@onecx/portal-integration-angular'
+import { BASE_URL, RemoteComponentConfig } from '@onecx/angular-remote-components'
 
-// import { AUTH_SERVICE, PhoneType, PortalMessageService, UserProfile } from '@onecx/portal-integration-angular'
-// import { NO_ERRORS_SCHEMA } from '@angular/core'
-// // import { UserProfileService } from '../user-profile.service'
-// import { TableModule } from 'primeng/table'
-// import { Router } from '@angular/router'
-// import { of, throwError } from 'rxjs'
+import { OneCXUserRolesPermissionsComponent } from './user-roles-permissions.component'
 // import { environment } from 'src/environments/environment'
+import { TranslateTestingModule } from 'ngx-translate-testing'
+import { RoleAPIService, UserAPIService, UserAssignment } from 'src/app/shared/generated'
+import { provideHttpClient } from '@angular/common/http'
 
-// describe('RolesPermissionsComponent', () => {
-//   let component: RolesPermissionsComponent
-//   let fixture: ComponentFixture<RolesPermissionsComponent>
+const userAssgnmnts: UserAssignment[] = [
+  {
+    roleName: 'role1',
+    productName: 'prod1',
+    applicationId: 'ocx-app1'
+  },
+  {
+    roleName: 'role2',
+    productName: 'prod2',
+    applicationId: 'ocx-app2'
+  }
+]
 
-//   const authServiceSpy = jasmine.createSpyObj('AUTH_SERVICE', ['getCurrentUser', 'getUserRoles'])
+const roles = ['role1', 'role2']
 
-//   const defaultCurrentUser: UserProfile = {
-//     userId: '15',
-//     person: {
-//       firstName: 'John',
-//       lastName: 'Doe',
-//       displayName: 'John Doe Display Name',
-//       email: 'john.doe@example.com',
-//       address: {
-//         street: 'Candy Lane',
-//         streetNo: '12',
-//         city: 'Candy Town',
-//         postalCode: '80-243',
-//         country: 'EN'
-//       },
-//       phone: {
-//         type: PhoneType.MOBILE,
-//         number: '123456789'
-//       }
-//     },
-//     memberships: [
-//       {
-//         application: 'app',
-//         roleMemberships: [
-//           {
-//             role: 'firstRole',
-//             permissions: [
-//               {
-//                 resource: 'resource1',
-//                 action: 'action1',
-//                 key: 'resource1#action1',
-//                 name: 'testPermission'
-//               },
-//               {
-//                 resource: 'resource2',
-//                 action: 'action2',
-//                 key: 'resource2#action2',
-//                 name: 'secondTestPermission'
-//               }
-//             ]
-//           },
-//           {
-//             role: 'secondRole',
-//             permissions: [
-//               {
-//                 resource: 'resource3',
-//                 action: 'action3',
-//                 key: 'resource3#action3',
-//                 name: 'thirdTestPermission'
-//               }
-//             ]
-//           }
-//         ]
-//       },
-//       {
-//         application: 'app2',
-//         roleMemberships: [
-//           {
-//             role: 'thirdRole',
-//             permissions: [
-//               {
-//                 resource: 'resource4',
-//                 action: 'action4',
-//                 key: 'resource4#action4',
-//                 name: 'fourthTestPermission'
-//               }
-//             ]
-//           }
-//         ]
-//       }
-//     ]
-//   }
+describe('OneCXUserRolesPermissionsComponent', () => {
+  let component: OneCXUserRolesPermissionsComponent
+  let fixture: ComponentFixture<OneCXUserRolesPermissionsComponent>
 
-//   const defaultUserRoles: string[] = ['secondRole', 'firstRole']
+  const userServiceSpy = {
+    getUserAssignments: jasmine.createSpy('getUserAssignments').and.returnValue(of({ stream: userAssgnmnts }))
+  }
+  const roleServiceSpy = {
+    searchRoles: jasmine.createSpy('searhRoles').and.returnValue(of({ stream: roles }))
+  }
 
-//   const userProfileServiceSpy = jasmine.createSpyObj<UserProfileService>('UserProfileService', ['getCurrentUserFromBE'])
+  const routerMock = jasmine.createSpyObj<Router>('Router', ['navigateByUrl'])
+  let baseUrlSubject: ReplaySubject<any>
 
-//   const messageServiceMock: jasmine.SpyObj<PortalMessageService> = jasmine.createSpyObj<PortalMessageService>(
-//     'PortalMessageService',
-//     ['info', 'error']
-//   )
+  beforeEach(waitForAsync(() => {
+    baseUrlSubject = new ReplaySubject<any>(1)
 
-//   const routerMock = jasmine.createSpyObj<Router>('Router', ['navigateByUrl'])
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [
+        TableModule,
+        TranslateTestingModule.withTranslations({
+          de: require('src/assets/i18n/de.json'),
+          en: require('src/assets/i18n/en.json')
+        }).withDefaultLanguage('en')
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: UserAPIService, useValue: userServiceSpy },
+        { provide: RoleAPIService, useValue: roleServiceSpy },
+        { provide: Router, useValue: routerMock },
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: BASE_URL,
+          useValue: baseUrlSubject
+        }
+      ]
+    })
+      .overrideComponent(OneCXUserRolesPermissionsComponent, {
+        set: {
+          imports: [TranslateTestingModule],
+          providers: [
+            { provide: UserAPIService, useValue: userServiceSpy },
+            { provide: RoleAPIService, useValue: roleServiceSpy },
+            { provide: AppConfigService }
+          ]
+        }
+      })
+      .compileComponents()
 
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [RolesPermissionsComponent],
-//       imports: [
-//         HttpClientTestingModule,
-//         TableModule,
-//         TranslateModule.forRoot({
-//           loader: {
-//             provide: TranslateLoader,
-//             useFactory: HttpLoaderFactory,
-//             deps: [HttpClient]
-//           }
-//         })
-//       ],
-//       schemas: [NO_ERRORS_SCHEMA],
-//       providers: [
-//         { provide: AUTH_SERVICE, useValue: authServiceSpy },
-//         { provide: UserProfileService, useValue: userProfileServiceSpy },
-//         { provide: PortalMessageService, useValue: messageServiceMock },
-//         { provide: Router, useValue: routerMock }
-//       ]
-//     }).compileComponents()
-//     authServiceSpy.getCurrentUser.and.returnValue(defaultCurrentUser)
-//     authServiceSpy.getUserRoles.and.returnValue(defaultUserRoles)
-//     messageServiceMock.info.calls.reset()
-//     messageServiceMock.error.calls.reset()
-//     routerMock.navigateByUrl.calls.reset()
-//   }))
+    baseUrlSubject.next('base_url_mock')
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RolesPermissionsComponent)
-//     component = fixture.componentInstance
-//     fixture.detectChanges()
-//     component.environment = environment
-//   })
+    userServiceSpy.getUserAssignments.calls.reset()
+    roleServiceSpy.searchRoles.calls.reset()
+    routerMock.navigateByUrl.calls.reset()
+  }))
 
-//   it('should create with correct data', () => {
-//     expect(component).toBeTruthy()
-//     // variables correctly assigned
-//     expect(component.personalInfo).toEqual(defaultCurrentUser.person)
-//     expect(component.memberships).toEqual(defaultCurrentUser.memberships!)
-//     // roles sorted
-//     expect(component.roles[0]).toEqual('firstRole')
-//     expect(component.roles[1]).toEqual('secondRole')
-//     // permissions created and sorted
-//     expect(component.permissionItems.length).toBe(4)
-//     expect(component.permissionItems[0]).toEqual({
-//       name: 'fourthTestPermission',
-//       key: 'resource4#action4',
-//       resource: 'resource4',
-//       action: 'action4',
-//       role: 'thirdRole',
-//       application: 'app2'
-//     })
-//     expect(component.permissionItems[1]).toEqual({
-//       name: 'secondTestPermission',
-//       key: 'resource2#action2',
-//       resource: 'resource2',
-//       action: 'action2',
-//       role: 'firstRole',
-//       application: 'app'
-//     })
-//     expect(component.permissionItems[2]).toEqual({
-//       name: 'testPermission',
-//       key: 'resource1#action1',
-//       resource: 'resource1',
-//       action: 'action1',
-//       role: 'firstRole',
-//       application: 'app'
-//     })
-//     expect(component.permissionItems[3]).toEqual({
-//       name: 'thirdTestPermission',
-//       key: 'resource3#action3',
-//       resource: 'resource3',
-//       action: 'action3',
-//       role: 'secondRole',
-//       application: 'app'
-//     })
-//   })
+  beforeEach(() => {
+    // component.environment = environment
+  })
 
-//   it('should filter table on applyGlobalFilter', () => {
-//     expect(component.permissionTableFilter).toBeDefined()
-//     component.permissionTableFilter!.nativeElement.value = 't'
-//     const event: Event = new Event('')
-//     spyOnProperty(event, 'target').and.returnValue(component.permissionTableFilter?.nativeElement)
-//     expect(component.permissionTable).toBeDefined()
-//     spyOn(component.permissionTable!, 'filterGlobal').and.callThrough()
-//     expect(component.permissionTable?.hasFilter()).toBe(false)
+  function initializeComponent() {
+    fixture = TestBed.createComponent(OneCXUserRolesPermissionsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  }
 
-//     component.applyGlobalFilter(event, component.permissionTable!)
-//     fixture.detectChanges()
+  it('should create with correct data', () => {
+    initializeComponent()
+    expect(component).toBeTruthy()
+  })
 
-//     expect(component.permissionTable?.hasFilter()).toEqual(true)
-//     expect(component.permissionTable?.filterGlobal).toHaveBeenCalledOnceWith('t', 'contains')
-//   })
+  describe('RemoteComponent initialization', () => {
+    it('should call ocxInitRemoteComponent with the correct config', () => {
+      const mockConfig: RemoteComponentConfig = {
+        appId: 'appId',
+        productName: 'prodName',
+        permissions: ['permission'],
+        baseUrl: 'base'
+      }
+      spyOn(component, 'ocxInitRemoteComponent')
 
-//   it('should navigate to root on close', () => {
-//     component.close()
+      component.ocxRemoteComponentConfig = mockConfig
 
-//     expect(routerMock.navigateByUrl).toHaveBeenCalledOnceWith('/')
-//   })
+      expect(component.ocxInitRemoteComponent).toHaveBeenCalledWith(mockConfig)
+    })
 
-//   it('should display error in console on refresh if not in production enviornment', () => {
-//     spyOn(console, 'error')
-//     component.refresh()
+    it('should initialize the remote component', (done: DoneFn) => {
+      initializeComponent()
 
-//     expect(console.error).toHaveBeenCalledOnceWith('Cannot refresh in non production mode')
-//   })
+      component.ocxInitRemoteComponent({
+        baseUrl: 'base_url'
+      } as RemoteComponentConfig)
 
-//   it('should load profile on refresh if in production environment if user was fetched', () => {
-//     component.environment = {
-//       production: true,
-//       BASE_PATH: '/portal-api'
-//     }
-//     fixture.detectChanges()
-//     userProfileServiceSpy.getCurrentUserFromBE.and.returnValue(
-//       of({
-//         userId: '',
-//         person: {}
-//       })
-//     )
-//     spyOn(component, 'loadProfileData')
-
-//     authServiceSpy.getCurrentUser.and.returnValue(defaultCurrentUser)
-//     authServiceSpy.getUserRoles.and.returnValue(defaultUserRoles)
-
-//     component.refresh()
-
-//     expect(component.loadProfileData).toHaveBeenCalledTimes(1)
-//     expect(messageServiceMock.info).toHaveBeenCalledOnceWith({
-//       summaryKey: 'ROLE_PERMISSIONS.MSG.PERMISSIONS_REFRESH_INFO'
-//     })
-//   })
-
-//   it('should display error on refresh if in production environment if user was not fetched', () => {
-//     component.environment = {
-//       production: true,
-//       BASE_PATH: '/portal-api'
-//     }
-//     const error = new Error()
-//     userProfileServiceSpy.getCurrentUserFromBE.and.returnValue(throwError(() => error))
-//     spyOn(console, 'error')
-
-//     component.refresh()
-
-//     expect(messageServiceMock.error).toHaveBeenCalledOnceWith({
-//       summaryKey: 'ROLE_PERMISSIONS.MSG.PERMISSIONS_REFRESH_ERROR'
-//     })
-//     expect(console.error).toHaveBeenCalledOnceWith(error)
-//   })
-
-//   it('should reset filter and table data on onClearFilterPermissionTable', () => {
-//     expect(component.permissionTableFilter).toBeDefined()
-//     component.permissionTableFilter!.nativeElement.value = 'value'
-//     expect(component.permissionTable).toBeDefined()
-//     spyOn(component.permissionTable!, 'clear').and.callThrough()
-//     spyOn(component, 'loadProfileData')
-
-//     component.onClearFilterPermissionTable()
-
-//     expect(component.permissionTableFilter?.nativeElement.value).toBe('')
-//     expect(component.permissionTable?.clear).toHaveBeenCalledTimes(1)
-//     expect(component.loadProfileData).toHaveBeenCalledTimes(1)
-//   })
-// })
+      baseUrlSubject.asObservable().subscribe((item) => {
+        expect(item).toEqual('base_url')
+        done()
+      })
+    })
+  })
+})
