caacf2762569dd29af895217702c2cae4cdb94a8
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-03-10 05:50:11 +0100

    feat: add ocx chip component (#340)
    * feat: add ocx-chip component
    
    * feat: add ocx-chip component
    
    * feat: clean env settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   KEYCLOAK_URL: 'http://keycloak-app/',
   KEYCLOAK_REALM: 'OneCX',
-  KEYCLOAK_CLIENT_ID: 'tkit-angular-example',
   TKIT_PORTAL_ID: 'ADMIN',
   skipRemoteConfigLoad: true,
   apiPrefix: 'bff'
