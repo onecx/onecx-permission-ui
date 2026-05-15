6790db7becbef880fa979867f1ad63e5255d0e05
Henry Täschner <129834483+HenryTaeschner@users.noreply.github.com>
2026-05-15 09:37:01 +0200

    Use pageSize 1000 for getting all roles

--- src/app/permission/app-detail/app-detail.component.ts
+++ src/app/permission/app-detail/app-detail.component.ts
@@ -345,7 +345,7 @@ export class AppDetailComponent implements OnInit, OnDestroy {
    * COLUMNS => Roles, ROWS => Permissions
    */
   private prepareSearchRolesAndPermissions(): void {
-    this.roles$ = this.roleApi.searchRoles({ roleSearchCriteria: {} }).pipe(
+    this.roles$ = this.roleApi.searchRoles({ roleSearchCriteria: { pageSize: this.pageSize } }).pipe(
       map((result: RolePageResult) => {
         return result.stream ? result.stream?.map((role) => role as PermissionRole) : []
       }),
