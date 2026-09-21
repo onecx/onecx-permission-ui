652803d6a65b6cf8a780a79e7c5903dc4e04fd59
Henry Taeschner <github@henry-taeschner.de>
2026-09-21 11:26:38 +0200

    fix: fix sonar issues

--- src/app/permission/app-detail/app-detail.component.spec.ts
+++ src/app/permission/app-detail/app-detail.component.spec.ts
@@ -127,7 +127,7 @@ const assgmtPageRes: AssignmentPageResult = {
   stream: [assgmt1, assgmt2]
 }
 
-fdescribe('AppDetailComponent', () => {
+describe('AppDetailComponent', () => {
   let component: AppDetailComponent
   let fixture: ComponentFixture<AppDetailComponent>
 
